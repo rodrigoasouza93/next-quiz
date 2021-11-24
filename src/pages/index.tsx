a7f8bface1132d51eec67d2e96562415f286785f
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2021-11-24 12:29:19 -0300

    refactor: update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { useRouter } from 'next/router';
-
-import { QuestionModel } from '../model/question';
 import { Button } from '../components/Button';
 
 import styles from '../styles/home.module.css';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
 export default function Home() {
-  const router = useRouter();
-
-  const [questionsIds, setQuestionsIds] = useState<number[]>([]);
-  const [question, setQuestion] = useState<QuestionModel>();
-  const [correctAnswers, setCorrectAnswers] = useState<number>(0);
-
-  const questionRef = useRef<QuestionModel>();
-
-  useEffect(() => {
-    loadQuestionsIds();
-  }, []);
-
-  useEffect(() => {
-    questionsIds.length > 0 && loadQuestion(questionsIds[0]);
-  }, [questionsIds]);
-
-  useEffect(() => {
-    questionRef.current = question;
-  }, [question]);
-
-  async function loadQuestionsIds() {
-    const response = await fetch(`${BASE_URL}/quiz`);
-    const questionsIdsData = await response.json();
-
-    setQuestionsIds(questionsIdsData);
-  }
-
-  async function loadQuestion(questionId: number) {
-    const response = await fetch(`${BASE_URL}/questions/${questionId}`);
-    const json = await response.json();
-
-    const newQuestion = QuestionModel.createUsingObject(json);
-    setQuestion(newQuestion);
-  }
-
-  function answeredQuestion(userAnsweredQuestion: QuestionModel) {
-    setQuestion(userAnsweredQuestion);
-    const hit = userAnsweredQuestion.hit;
-
-    if (hit) {
-      setCorrectAnswers(correctAnswers + 1);
-    }
-  }
-
-  function getNextQuestionid() {
-    const nextIndex = questionsIds.indexOf(question.id) + 1;
-    return questionsIds[nextIndex];
-  }
-
-  function goToNextQuestion(nextId: number) {
-    loadQuestion(nextId);
-  }
-
-  function end() {
-    router.push({
-      pathname: '/result',
-      query: {
-        total: questionsIds.length,
-        corrects: correctAnswers
-      }
-    });
-  }
-
-  function goToNextStep() {
-    const nextId = getNextQuestionid();
-
-    nextId ? goToNextQuestion(nextId) : end();
-  }
-
   return (
     <div className={styles.home}>
       <h1>Bem vindo ao jogo de questionário</h1>
